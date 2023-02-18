03e6e93be97031b0337298f4bee6bf7f285bfbb4
Ik0n <Ik0nChannel1@mail.ru>
2023-02-18 20:45:56 +0300

    Добавил фрагменты MonthFragment.kt и ProgramsListFragment.kt и настроил в MainActivity.kt переходы между ними с помощью bottomNavigationBar

--- app/src/main/java/ru/ikon/trainingdairy/MainActivity.kt
+++ app/src/main/java/ru/ikon/trainingdairy/MainActivity.kt
@@ -2,10 +2,46 @@ package ru.ikon.trainingdairy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import ru.ikon.trainingdairy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() { return _binding!! }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_holder, ProgramsListFragment.newInstance())
+                .commit()
+        }
+
+        initBottomNavigationBar()
     }
+
+    private fun initBottomNavigationBar() {
+        binding.bottomNavigationBar.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.action_bottom_navigation_bar_programs -> startFragment(ProgramsListFragment.newInstance())
+                R.id.action_bottom_navigation_bar_trainings -> startFragment(MonthFragment.newInstance())
+            }
+
+            true
+        }
+    }
+
+    private fun startFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_holder, fragment)
+            .commit()
+    }
+
 }
